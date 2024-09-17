209873ae54773a1a95e1259f0a8b871006ed2f18
Lukas Rennhofer <132039008+LL-Program@users.noreply.github.com>
2024-09-17 19:31:22 +0200

    Update main.py

--- src/Editor/main.py
+++ src/Editor/main.py
@@ -1,6 +1,8 @@
 #Dominik Pascher
+from customtkinter import *
 
 class CHIFEditor:
     def __init__(self,engine,win_size):
         self.WIN_SIZE = win_size
-        #.....
+        self.Engine = engine
+        #...
