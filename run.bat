b8b7267c70784bf43f1feaa0b17bb64aa6b70496
Lukas Rennhofer <132039008+LL-Program@users.noreply.github.com>
2024-10-30 12:52:04 +0100

    Update run.bat

--- run.bat
+++ run.bat
@@ -21,9 +21,9 @@ if %errorlevel% neq 0 (
 ::python -m pip install --upgrade pip
 
 :: Install required packages
-echo Installing required packages: moderngl, numpy, pygame, PyGLM, pywavefront, customtkinter(maybe tkinter), matplotlib...
+echo Installing required packages
 echo This could take a while if you run it for the first time...
-pip install moderngl numpy pygame PyGLM pywavefront customtkinter
+pip install -r requirements.txt
 
 :: Check if installation was successful
 if %errorlevel% neq 0 (
@@ -33,8 +33,8 @@ if %errorlevel% neq 0 (
 )
 
 :: Run the main Python script
-echo Running MainEngine...
-python src/Engine/MainEngine.py
+echo Running mainEngineRuntime...
+python mainEngineruntime.py
 
 :: Pause to see output if needed
 pause
